b64157d89132c351a296e818e651fcb80393fe19
LAGM <luisiyi1997@gmail.com>
2025-07-05 21:14:12 -0700

    ProductoControlador class added and ProductoServicio name class modified
    -added "ProductoControlador" class set RestController to say that it is a rest controller, we set @RequestMapping tag with inventario-app name, this will be adding in the URL, we set @CrossOrigin tag this will allow get request from the Front end Angular that will be the 4200 port, we se productoServicio object as Autowired (Dependency injenction from the ProductoServicio class.), and finally we added the "obtenerProductos" method that will return all the productos from the DB, this method has a tag that is @GetMapping("/productos"), and this will be the final URL to get the productos.
    -modiefied from "ProductoService" to "ProductoServicio" class name.

--- src/main/java/gm/inventarios/servicio/ProductoServicio.java
+++ src/main/java/gm/inventarios/servicio/ProductoServicio.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class ProductoService implements IProductoServicio{
+public class ProductoServicio implements IProductoServicio{
 
     @Autowired
     private ProductoRepositorio productoRepositorio;

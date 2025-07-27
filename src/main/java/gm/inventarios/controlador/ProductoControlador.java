b3a160a58d0cee51e9b7d46540a29c6e97547a06
LAGM <luisiyi1997@gmail.com>
2025-07-26 18:34:10 -0700

    -Delete producto method added
    -added "eliminarProducto" method, this one uses @DeleteMapping that indicates that use delete requests, also this method will return a "Map" String, boolean, this receive the id from the URL, with the Id we get the producto from the DB,
    -if exist: we proceed to delete the producto with its id using the "eliminarProducto" method from the "productoServicio" service, then we create the Map variable called respuesta that is "eliminado": TRUE, and finally return the ResponseEntity.ok(respuesta) recently created.
    -if not exist: we just throw an error with RecursoNoEncontradoExcepcion("No se encontró el id del producto: " + id) that is a "404 Not Found"

--- src/main/java/gm/inventarios/controlador/ProductoControlador.java
+++ src/main/java/gm/inventarios/controlador/ProductoControlador.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("inventario-app") //http:localhost:8080/inventario-app
@@ -58,5 +60,18 @@ public class ProductoControlador {
         //Guardamos la informacion
         this.productoServicio.guardarProducto(producto);
         return ResponseEntity.ok(producto);
+    }
+
+    @DeleteMapping("/productos/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarProducto(@PathVariable int id){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if (producto != null){
+            this.productoServicio.eliminarProducto(producto.getIdProducto());
+            Map<String, Boolean> respuesta = new HashMap<>();
+            respuesta.put("eliminado", Boolean.TRUE);
+            return ResponseEntity.ok(respuesta);
+        }else {
+            throw new RecursoNoEncontradoExcepcion("No se encontró el id del producto: " + id);
+        }
     }
 }
